6bcef9b6feebed1e90da3cd24df2610140d25dfa
Duchicus <mpiernash@gmail.com>
2024-05-03 11:51:08 +0200

    creacion de rutas para comprobacion de CRUD de reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,5 +4,7 @@ const router = express.Router()
 
 router.post("/",ReviewController.create)
 router.get("/",ReviewController.getAll)
+router.put("/id/:id",ReviewController.update)
+router.delete("/id/:id",ReviewController.delete)
 
 module.exports = router
