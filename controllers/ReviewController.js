51939654f1dea5449532a6a12e8ed166e1db9a49
Duchicus <mpiernash@gmail.com>
2024-05-03 11:39:42 +0200

    CRUD de review

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,9 +1,9 @@
-const { review, User } = require("../models/index.js");
+const { Review, User } = require("../models/index.js");
 
 const ReviewController = {
     async create(req, res) {
         try {
-            await review.create(req.body);
+            await Review.create(req.body);
             res.status(201).send({msg:"Producto creado con exito"})
         } catch (error) {
             console.error(error);
@@ -11,10 +11,34 @@ const ReviewController = {
         }
     },async getAll(req,res){
         try {
-            const rev = await review.findAll({
+            const rev = await Review.findAll({
                 include : [{model:User,attributes:["email"]}]
             })
             res.send(rev)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg:"Error de servidor"})
+        }
+    },async update(req, res) {
+        try {
+            await Review.update(req.body, {
+                    where: {
+                        id: req.params.id
+                    }
+            });
+            res.send("Review actualizada con exito")
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg:"Error de servidor"})
+        }
+    },async delete(req, res) {
+        try {
+            await Review.destroy({
+                    where: {
+                        id: req.params.id
+                    }
+            });
+            res.send("Review eliminada con exito")
         } catch (error) {
             console.error(error);
             res.status(500).send({msg:"Error de servidor"})
