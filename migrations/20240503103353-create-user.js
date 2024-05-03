b31ff069f6410072be62a23a2d1edb832f3c289b
Duchicus <mpiernash@gmail.com>
2024-05-03 12:36:24 +0200

    actualizacion del modelo users con unique email

--- migrations/20240503103353-create-user.js
+++ migrations/20240503103353-create-user.js
@@ -10,7 +10,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique:true
       },
       password: {
         type: Sequelize.STRING
