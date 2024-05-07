e9a6d95dd12b75af00eac56ee4edab97e83882d9
Duchicus <mpiernash@gmail.com>
2024-05-07 10:01:05 +0200

    resolucion rutas Users

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const UserController = require("../controllers/UserController")
-const {authentication} = require('../middleware/authentication')
+const {authentication, isAdmin} = require('../middleware/authentication')
 const router = express.Router()
 
 router.post("/register",UserController.register)
 router.post("/login",UserController.login)
 router.delete('/logout',authentication,UserController.logout)
-router.get('/',UserController.getAll)
-router.get('/user',authentication, UserController.getContent)
+router.get('/',authentication,isAdmin,UserController.getAll)
+router.get('/user',authentication,isAdmin, UserController.getContent)
 router.get('/confirm/:emailToken',UserController.confirm)
 
 module.exports = router
